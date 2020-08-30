2428e1746eae4c755a1573aef47fede8f7800a5d
piaoyehong1107 <piaoyehong1107@gmail.com>
2020-08-29 19:31:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function getFirstSelector(selector){
+   return document.querySelector(selector)
+}
+function nestedTarget(){
+   return document.querySelector('#nested .target')
+}
 
+function increaseRankBy(n){
+  const lis= document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  for(const ele of lis){ele.innerHTML=parseInt(ele.innerHTML)+n}
+}
+
+function deepestChild(){
+    const final= document.querySelector('div#grand-node div div div div')
+    return final
+}
